cd573a68af5d90b92ddf8724444e9d006318f207
K <katerynachi5@gmail.com>
2023-09-20 12:19:32 +0300

    Add DTOs, Service, Repository, Controller

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,11 +1,13 @@
 package com.example.onlinebookstore;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
 }
