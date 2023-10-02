87a7b5d52056ce692d449b775f5884b399e37f7f
K <katerynachi5@gmail.com>
2023-10-02 21:43:41 +0300

    Add CustomGlobalExceptionHandler, add validation annotations

--- src/main/java/com/example/onlinebookstore/dto/BookSearchParameters.java
+++ src/main/java/com/example/onlinebookstore/dto/BookSearchParameters.java
@@ -1,9 +1,13 @@
 package com.example.onlinebookstore.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BookSearchParameters {
+    @NotNull
     private String[] titles;
+
+    @NotNull
     private String[] authors;
 }
